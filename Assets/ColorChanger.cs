f154f995efe7085ec1bb89ef1547ea8ad8733b79
Joel <joel.andersson@abbindustrigymnasium.se>
2022-01-27 13:11:00 +0100

    ChangeColor on space

--- Assets/ColorChanger.cs
+++ Assets/ColorChanger.cs
@@ -9,19 +9,23 @@ public class ColorChanger : MonoBehaviour
     public TMP_Text m_TextComponent;
 
     int textNumber;
+    int textColor;
 
 
     void Start()
     {
         m_TextComponent = GetComponent<TMP_Text>();
+        
 
         m_TextComponent.text = "Red";
+        m_TextComponent.color = new Color32(255, 128, 0, 255);
 
     }
 
     void Update()
     {
-
+        if (Input.GetKeyDown("space"))
+        {
         textNumber = Random.Range(1, 4);
         switch (textNumber)
         {
@@ -40,6 +44,26 @@ public class ColorChanger : MonoBehaviour
             default:
                 m_TextComponent.text = "Red";
                 break;
+        }
+        textColor = Random.Range(1, 4);
+        switch (textColor)
+        {
+            case 1:
+                m_TextComponent.color = new Color32(255, 0, 0, 255);
+                break;
+            case 2:
+                m_TextComponent.color = new Color32(0, 0, 255, 255);
+                break;
+            case 3:
+                m_TextComponent.color = new Color32(255, 255, 0, 255);
+                break;
+            case 4:
+                m_TextComponent.color = new Color32(0, 255, 0, 255);
+                break;
+            default:
+                m_TextComponent.color = new Color32(255, 0, 0, 255);
+                break;
+        }
         }
     }
 }
